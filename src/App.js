cf10dee42707a666bd802b9bb7a8632318cf4431
Shailendra Nagori <shelunagori@gmail.com>
2023-02-28 11:26:16 +0000

    setting up routes and nav bar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import CategoryGrid from './components/category-grid/category-grid.component';
+import { Routes, Route } from "react-router";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
-const App = () => {
-
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  
-  return <CategoryGrid categories={categories}/>
+const Shop = () => {
+  return (
+    <h1>HEllo</h1>
+  )
 }
 
+const App = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Navigation />}>
+            <Route index element={<Home />} />
+            <Route path='shop' element={<Shop />} />            
+        </Route>
+    </Routes>
+  );
+}
 export default App;
